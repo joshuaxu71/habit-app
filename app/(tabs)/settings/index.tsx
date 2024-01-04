3b6359e73876d72d43233c5a02f03f26efabea9c
joshuaxu71 <joshuaxu71@gmail.com>
2024-01-04 16:22:48 +0700

    create SettingsListItem for the Settings page

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -4,12 +4,12 @@ import SettingsListItem, { SettingsItemProps } from '../../../components/Setting
 
 export default function SettingsScreen() {
   const settingsData: SettingsItemProps[] = [
-    { id: '1', title: 'Synchronization', showSwitch: false, expandable: false},
-    { id: '2', title: 'Time Format', showSwitch: true, expandable: true},
-    { id: '3', title: 'Enable Notifications', showSwitch: true, expandable: true},
-    { id: '4', title: 'Notification Reminder', showSwitch: false, expandable: false},
-    { id: '5', title: 'Time Category', showSwitch: false, expandable: false},
-    { id: '6', title: 'Posting Rule', showSwitch: false, expandable: false},
+    { id: '1', title: 'Synchronization', showSwitch: false},
+    { id: '2', title: 'Time Format', description: "Customize the time shown between 12H format (e.g. 1:00 PM) and 24H format (13:00)", showSwitch: true},
+    { id: '3', title: 'Enable Notifications', description: "Enable notifications for reminders", showSwitch: true},
+    { id: '4', title: 'Notification Reminder', description: "Set how many minutes before the activity should the reminder be sent on (e.g. activity is at 13:00, and the reminder is reminder is 5 minutes, so the notification will be sent at 12:55)", showSwitch: false},
+    { id: '5', title: 'Time Category', showSwitch: false},
+    { id: '6', title: 'Posting Rule', showSwitch: false},
   ];
 
   const renderSettingItem = ({ item }: { item: SettingsItemProps }) => (
--- components/SettingsListItem.tsx
+++ components/SettingsListItem.tsx
@@ -1,22 +1,22 @@
+import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
-import { Pressable, StyleSheet, Text, View, useColorScheme } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, useColorScheme } from 'react-native';
 import { Switch } from 'react-native-paper';
 import Colors from '../constants/Colors';
-
 
 export interface SettingsItemProps {
   id: string,
   title: string,
   description?: string,
   showSwitch: boolean,
-  expandable: boolean,
   routeName?: string;
 }
 
-export default function SettingsListItem({ id, title, description, showSwitch, expandable, routeName}: SettingsItemProps) {
-  const [isChecked, setChecked] = useState(false);
-  const [isExpanded, setExpanded] = useState(false);
+export default function SettingsListItem({ id, title, description, showSwitch, routeName}: SettingsItemProps) {
+  const expandable = description != null
+  const [checked, setChecked] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   const navigation = useNavigation();
 
   const colorScheme = useColorScheme();
@@ -25,41 +25,94 @@ export default function SettingsListItem({ id, title, description, showSwitch, e
 
   const handlePress = (routeName: string = "") => {
     if (routeName != "") {
-      // Navigate to the specified route when the card is pressed
       navigation.navigate(routeName);
     } else if (expandable) {
-      setExpanded(!isExpanded);
-    }    
+      setExpanded(!expanded);
+    }
   };
 
   const renderSettingItem = (
-    <Pressable onPress={() => handlePress(routeName)}>
-      <View
-        style={[
-          styles.settingItemContainer,
-          { backgroundColor: itemBackGroundColor }, // Set background color based on color scheme
-        ]}
-      >
-        <Text
-          style={{ color: textColor }} // Set text color based on color scheme
-        >
-          {title}
-        </Text>
-        {showSwitch && <Switch value={isChecked} onValueChange={() => setChecked(!isChecked)} />}
-        {/* {isExpanded && <Paragraph style={{ color: textColor }}>{description}</Paragraph>} */}
+    <View style={styles.settingsContainer}>
+      <View style={styles.settingsItemContainer}>
+        <View style={[
+          styles.settingsItemTitleContainer,
+          expanded ? styles.settingsItemTitleContainerExpanded : null
+        ]}>
+          <View>
+            <TouchableOpacity disabled={!expandable} onPress={() => handlePress(routeName)} style={styles.settingsItemTitleIconContainer}> 
+              {expandable && (
+                expanded 
+                ? <AntDesign name="up" size={18} color={textColor} style={ styles.settingsItemIcon } /> 
+                : <AntDesign name="down" size={18} color={textColor} style={ styles.settingsItemIcon } />
+              )}
+              <Text style={[
+                styles.settingsItemTitle,
+                { color: textColor }
+              ]}> 
+                {title} 
+              </Text> 
+            </TouchableOpacity> 
+          </View>
+          {showSwitch && (
+            <Switch color="black" style={styles.settingsItemSwitch} value={checked} onValueChange={() => setChecked(!checked)} />
+          )}
+        </View>
+        {expanded && (
+          <Text style={[
+            styles.settingsItemDescription,
+            { color: textColor }
+          ]}>
+            {description}
+          </Text>
+        )} 
       </View>
-    </Pressable>
+    </View>
   );
 
   return renderSettingItem;
 }
 
 const styles = StyleSheet.create({
-  settingItemContainer: {
+  settingsContainer: {
+    padding: 10,
+  },
+  settingsItemContainer: {
+    
+  },
+  settingsItemTitleContainer: {
+    padding: 5,
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center', // Align items in the center vertically
-    padding: 10,
-    height: 50, // Set a fixed height for the container
+    alignItems: 'center',
+    minHeight: 40,
+    width: '100%',
+    backgroundColor: 'gray',
+    borderRadius: 10,
+  },
+  settingsItemTitleContainerExpanded: {
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  settingsItemTitleIconContainer: {
+    marginLeft: 5,
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  settingsItemIcon: {
+    marginRight: 5,
+    justifyContent: 'flex-start',
+  }, 
+  settingsItemTitle: {
+    fontSize: 18, 
+  }, 
+  settingsItemSwitch: {
+    height: 10,
+  },
+  settingsItemDescription: { 
+    padding: 10, 
+    fontSize: 14, 
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: 'darkgray',
   },
 });
